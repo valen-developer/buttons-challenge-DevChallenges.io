a79fab2d9631e4219eada3e947ed814fbc804094
Valentin Valverde <valentin@valentinvalverde.com>
2021-01-27 02:54:45 +0100

    buttons part

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ButtonsComponent } from './components/pages/buttons/buttons.component';
+import { InputsComponent } from './components/pages/inputs/inputs.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'buttons', component: ButtonsComponent },
+  { path: 'inputs', component: InputsComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'buttons' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideBarComponent } from './components/shared/side-bar/side-bar.component';
+import { ButtonsComponent } from './components/pages/buttons/buttons.component';
+import { InputsComponent } from './components/pages/inputs/inputs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideBarComponent,
+    ButtonsComponent,
+    InputsComponent
   ],
   imports: [
     BrowserModule,
