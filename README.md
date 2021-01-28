f2695e1006858ee661e189cf94ca4e60bc9a3c2f
Valentin Valverde <valentin@valentinvalverde.com>
2021-01-28 02:35:56 +0100

    refactor and readme

---
+++ README.md
@@ -0,0 +1,38 @@
+# :bowtie: A [DevChallenge.io](https://devchallenges.io) challenge
+
+Today, we are going doing a challenge of DevChallenges.io. It´s on front-end section (although I think that it´a a design challenge, maybe).
+
+It´s simple, We just need to make some buttons: different sizes, colors, shapes...
+
+Also I´ve made a responsive navbar (sidebar rather) although I have to get better the code organization.
+
+# Run project in localhost
+
+1. First clone this repo:
+
+```
+git clone
+```
+
+2. Install Node:
+   [Nodejs](https://nodejs.org/en/)
+
+3. Run project:
+   - If you don´t have Angular CLI installed:
+   ```
+   npx -p @angular/cli@10 ng serve --port <port you prefer>
+   ```
+   - If you have Angular CLI installed:
+   ```
+   ng serve --port <port you prefer>
+   ```
+   - If you wanna to see the website in your tablet or phone add --host in the previous step
+
+# :eyes: Visit de Demo web
+
+Go to [demo web]()
+
+# :camera: Screenshots
+
+<img src="./screenshots/buttons-challenge-desktop.png" width="300" >   
+<img src="./screenshots/buttons-challenge-mobile.jpg" height="150" >
--- src/app/components/shared/side-bar/side-bar.component.ts
+++ src/app/components/shared/side-bar/side-bar.component.ts
@@ -3,13 +3,51 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
-  styleUrls: ['./side-bar.component.css']
+  styleUrls: ['./side-bar.component.css'],
 })
 export class SideBarComponent implements OnInit {
+  private isSideBarShown = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public manageSideBarShow(): void {
+    if (this.isSideBarShown) this.hiddeSideBar();
+    else this.showSideBar();
   }
 
+  private showSideBar(): void {
+    const topLine = document.getElementById('top-line');
+    const middleLine = document.getElementById('middle-line');
+    const bottomLine = document.getElementById('bottom-line');
+
+    const sideBar = document.getElementById('side-bar');
+
+    sideBar.style.left = '0';
+
+    middleLine.style.opacity = '0';
+
+    topLine.classList.add('top-line');
+    bottomLine.classList.add('bottom-line');
+
+    this.isSideBarShown = true;
+  }
+
+  private hiddeSideBar(): void {
+    const topLine = document.getElementById('top-line');
+    const middleLine = document.getElementById('middle-line');
+    const bottomLine = document.getElementById('bottom-line');
+
+    const sideBar = document.getElementById('side-bar');
+
+    sideBar.style.left = '-100vw';
+
+    middleLine.style.opacity = '1';
+
+    topLine.classList.remove('top-line');
+    bottomLine.classList.remove('bottom-line');
+
+    this.isSideBarShown = false;
+  }
 }
